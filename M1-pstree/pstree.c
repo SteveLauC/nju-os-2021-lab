a6e955a66436dd3dbf2bfa6c490304ac009d72b3
tracer-nju <tracer@nju.edu.cn>
2022-03-05 12:35:15 +0800

    Static hostname: steve-MacBookPro          Icon name: computer-laptop            Chassis: laptop         Machine ID: 53eae4f13b8a4338bbc7c797dda60d96            Boot ID: f111879a801f4bb0aa71cf992032792c   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-30-generic       Architecture: x86-64  12:35:15 up 7 days, 13:17,  9 users,  load average: 0.63, 0.69, 0.62

--- M1-pstree/pstree.c
+++ M1-pstree/pstree.c
@@ -222,7 +222,7 @@ void numeric_sort(processes * const p, const int index) {
         int ptr = 0;
         for (int j = indices_of_child_proc[ptr]; ptr < amount_of_child_proc-i-1; ptr++) {
             j = indices_of_child_proc[ptr];
-            if (p->p_array[j].pid > p->p_array[indices_of_child_proc[ptr+1]]) {
+            if (p->p_array[j].pid > p->p_array[indices_of_child_proc[ptr+1]].pid) {
                 int tmp_pid = p->p_array[j].pid;
                 p->p_array[j].pid = p->p_array[indices_of_child_proc[ptr+1]].pid;
                 p->p_array[indices_of_child_proc[ptr+1]].pid = tmp_pid;
